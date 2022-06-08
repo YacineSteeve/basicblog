53bc349f0fd4c0c3509c2bd29f1312a717194e46
YacineSteeve <steeveboukari9@gmail.com>
2022-06-09 01:15:45 +0200

    feat(bloggers): Display the age of blogger's latest post.
    Update blogger_list.html accordingly  to the new feature.
    Add a static method to models.py to convert timedelta into dict of timestamps.

--- weblog/models.py
+++ weblog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
+from collections import OrderedDict
 import os
 import datetime
 
@@ -9,6 +10,28 @@ import datetime
 def upload_to_user_directory(instance, filename) -> str:
     new_filename = f'{instance.user.username}.{filename[-3:]}'
     return os.path.join(instance.user.username, 'avatar', new_filename)
+
+
+def convert_timedelta_to_dict(delta: datetime.timedelta) -> dict:
+    seconds = delta.seconds % 60
+    minutes = ((delta.seconds - seconds) // 60) % 60
+    hours = ((delta.seconds - minutes * 60) // 3600) % 86400
+    days = delta.days % 7
+    weeks = ((delta.days - days) // 7) % 4
+    months = ((delta.days - weeks * 7) // 30) % 12
+    years = (delta.days - months * 30) // 365
+
+    parsed_delta = OrderedDict({
+        'seconds': seconds,
+        'minutes': minutes,
+        'hours': hours,
+        'days': days,
+        'weeks': weeks,
+        'months': months,
+        'years': years
+    })
+
+    return parsed_delta
 
 
 class BlogPost(models.Model):
@@ -22,16 +45,12 @@ class BlogPost(models.Model):
         ordering = ['-post_date', 'author', 'title']
         get_latest_by = '-post_date'
 
-    def get_blog_post_age(self) -> datetime.timedelta:
-        delta = timezone.now() - self.post_date
-        times = {
-            'seconds': delta.seconds,
-            'minutes': delta.minutes,
-            'hours': delta.hours,
-            'days': delta.days,
-            'weeks': delta.weeks
-        }
-        return delta
+    def get_blog_post_age(self) -> str:
+        delta = convert_timedelta_to_dict(timezone.now() - self.post_date)
+        for time, value in list(delta.items())[:0:-1]:
+            if value != 0:
+                return f'{value} {time} ago' if value >= 2 else f'{value} {time[:-1]} ago'
+        return 'now'
 
     def get_absolute_url(self) -> str:
         return reverse('blog-post-detail', args=[self.id])
