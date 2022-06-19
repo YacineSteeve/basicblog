12ecbc599396ca671905634c42986be161473e47
YacineSteeve <steeveboukari9@gmail.com>
2022-06-19 11:25:12 +0200

    feat(admin): Add inline views to Blogger, BlogPost and Comment list views.

--- weblog/admin.py
+++ weblog/admin.py
@@ -2,26 +2,41 @@ from django.contrib import admin
 from .models import Blogger, BlogPost, Comment, Answer, Category
 
 
+admin.site.register(Category)
+admin.site.register(Answer)
+
+
+class BlogPostInline(admin.TabularInline):
+    model = BlogPost
+    extra = 0
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    extra = 0
+
+
 @admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        CommentInline,
+    ]
 
 
 @admin.register(Blogger)
 class BloggerAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        BlogPostInline,
+    ]
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Answer)
-class AnswerAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        AnswerInline,
+    ]
